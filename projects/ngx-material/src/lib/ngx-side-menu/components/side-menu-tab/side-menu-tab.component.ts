be20fa723897c651269ea46cb2155eeea102b0e9
Frederik Sørensen <frederikjs1@hotmail.com>
2024-02-28 10:19:46 +0100

    Improve side-menu badges

--- projects/ngx-material/src/lib/ngx-side-menu/components/side-menu-tab/side-menu-tab.component.ts
+++ projects/ngx-material/src/lib/ngx-side-menu/components/side-menu-tab/side-menu-tab.component.ts
@@ -5,6 +5,7 @@ import {
 import {NgxSideMenuTabContext} from "../../models/menu-tab-context";
 import {ThemePalette} from "@angular/material/core";
 import {RenderSourceDirective} from "@juulsgaard/ngx-tools";
+import {isString} from "@juulsgaard/ts-tools";
 
 @Component({
   selector: 'ngx-side-menu-tab',
@@ -18,7 +19,17 @@ export class SideMenuTabComponent extends NgxSideMenuTabContext {
   readonly slug: InputSignal<string> = input.required<string>();
   readonly name: InputSignal<string | undefined> = input<string>();
   readonly icon: InputSignal<string | undefined> = input<string>();
-  readonly badge: InputSignal<string | undefined> = input<string>();
+  readonly badge: InputSignalWithTransform<string | undefined, string | number | undefined | null> = input(
+    undefined,
+    {
+      transform: (badge: string | number | undefined | null) => {
+        if (badge == null) return undefined;
+        if (!badge) return undefined;
+        if (isString(badge)) return badge;
+        return badge.toFixed(0);
+      }
+    }
+  );
   readonly badgeColor: InputSignal<ThemePalette | undefined> = input<ThemePalette>();
 
   readonly disabled: InputSignalWithTransform<boolean, unknown> = input(false, {transform: booleanAttribute});
