c20e220bea26d9da223379509b53b87129ee5eb4
Frederik Sørensen <frederikjs1@hotmail.com>
2024-03-13 23:49:01 +0100

    Revert form dialog workaround

--- projects/ngx-forms/src/lib/ngx-form-dialog/ngx-form-dialog.module.ts
+++ projects/ngx-forms/src/lib/ngx-form-dialog/ngx-form-dialog.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {AsyncPipe, NgIf} from '@angular/common';
 import {FormDialogComponent} from "./components/form-dialog/form-dialog.component";
-import {FalsyPipe, LoadingDirective, NgxIfDirective, NgxRenderingModule} from "@juulsgaard/ngx-tools";
+import {LoadingDirective, NgxRenderingModule} from "@juulsgaard/ngx-tools";
 import {FormInputDirective} from "../../directives";
 import {FormDialogDirective} from "./directives/dialog-form.directive";
 import {MatButton} from "@angular/material/button";
@@ -24,8 +24,6 @@ import {MatButton} from "@angular/material/button";
     FormInputDirective,
     MatButton,
     LoadingDirective,
-    FalsyPipe,
-    NgxIfDirective
   ]
 })
 export class NgxFormDialogModule { }
